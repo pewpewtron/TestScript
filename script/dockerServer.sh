66d9bc312740124aa4431c4b627ebb3f94edd72f
Devtian Dicky <devtiandicky12@outlook.com>
2021-10-25 17:25:34 +0800

    fix permision issue and error during runing script change docker compose to v 1.21.2

--- script/dockerServer.sh
+++ script/dockerServer.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-set -eu -o pipefail
-
-sudo -n true
-test $? -eq 0 || exit 1
-
 #Update Package
 echo Update Pakcage
 apt-get update
 
 # Add Docker repo
 echo add Docker repo
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
@@ -21,13 +16,13 @@ sudo apt update
 apt-cache policy docker-ce
 
 #Install Docker
-echo Intsall Docker
+echo Intsall Docker -y
 sudo apt install docker-ce
 
 echo install docker compose
 #Downlaod docker compose from git repo
 echo download docker compose repo
-sudo curl -L https://github.com/docker/compose/releases/download/2.0.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 #set permissions
 echo add permissions
