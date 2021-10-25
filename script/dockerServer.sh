7611f4d5bd6e4fd9ff140d971bcc635611e67d39
Devtian Dicky <devtiandicky12@outlook.com>
2021-10-25 22:41:16 +0800

    remove sudo command in script

--- script/dockerServer.sh
+++ script/dockerServer.sh
@@ -12,27 +12,27 @@ apt-get update
 
 # Add Docker repo
 echo add Docker repo
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
 #Update pakcage
 echo Update Package with docker repo
-sudo apt update
+apt update
 apt-cache policy docker-ce
 
 #Install Docker
 echo Intsall Docker -y
-sudo apt install docker-ce
+apt install docker-ce
 
 echo install docker compose
 #Downlaod docker compose from git repo
 echo download docker compose repo
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 #set permissions
 echo add permissions
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Check Docker and compose version
 docker -v
--- script/setupVM.sh
+++ script/setupVM.sh
@@ -16,8 +16,8 @@ apt-get install nginx -y
 #Install Database
 echo Install Database
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
-sudo apt-get update
-sudo apt-cache policy mysql-server 
+dpkg -i mysql-apt-config_0.8.12-1_all.deb
+apt-get update
+apt-cache policy mysql-server 
 apt install -f mysql-client=5.7.33-1ubuntu18.04 mysql-community server=5.7.33-1ubuntu18.04 mysql-server=5.7.33-1ubuntu18.04
-sudo apt-get install mysql-server -y
+apt-get install mysql-server -y
